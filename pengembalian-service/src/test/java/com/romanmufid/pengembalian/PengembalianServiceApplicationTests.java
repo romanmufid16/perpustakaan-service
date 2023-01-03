ae99370ee7266a0d07677498084c03bc77e5f473
romanmufid16 <romankeanumufid@gmail.com>
2023-01-03 20:04:09 +0700

    Roman Keanu Mufid

---
+++ pengembalian-service/src/test/java/com/romanmufid/pengembalian/PengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanmufid.pengembalian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
